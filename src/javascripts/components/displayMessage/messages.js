2818d3fd61f8eaeebdbe5246c306d4b3843923be
Greg Stephen <GregAStephen@gmail.com>
2019-04-29 21:22:55 -0500

    changed color of slider button and other things

--- src/javascripts/components/displayMessage/messages.js
+++ src/javascripts/components/displayMessage/messages.js
@@ -7,12 +7,12 @@ let messages = [];
 const messageDomStringBuilder = () => {
   let domString = '';
   messages.forEach((message) => {
-    domString += '<div class="media message">';
+    domString += '<div class="media message container">';
     domString += `<img src="${message.image}" class="mr-3 userImage" alt="...">`;
     domString += '<div class="media-body">';
-    domString += '<div class="media-header">';
-    domString += `<h5 class="userName mt-0">${message.username}</h5>`;
-    domString += `<h6 class= "timeStamp mt-0">${message.timeStamp}</h6>`;
+    domString += '<div class="media-header row justify-content-start">';
+    domString += `<h5 class="userName mt-0 col-auto">${message.username}</h5>`;
+    domString += `<p class= "timeStamp mt-0 col">${message.timeStamp}</p>`;
     domString += '</div>';
     domString += `<p>${message.message}</p>`;
     domString += '</div>';
