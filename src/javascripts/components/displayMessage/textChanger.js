be5c13803c5c97bd1a3af2fecbbd9ded40d14ed0
Dev Davis <sean.dev.davis@gmail.com>
2019-05-02 16:25:41 -0500

    WIP

--- src/javascripts/components/displayMessage/textChanger.js
+++ src/javascripts/components/displayMessage/textChanger.js
@@ -3,18 +3,24 @@ import $ from 'jquery';
 const p = $('p').addClass('bigText');
 const h5 = $('h5').addClass('bigText');
 
-$('#largeText').click(() => {
-  if (p.hasClass('bigText')) {
-    $('p').toggleClass().css('font-size', '32px');
-  } else {
-    $('p').toggleClass().css('font-size', '16px');
-  }
-});
+const fontFlipper = () => {
+  $('#largeText').click(() => {
+    if (p.hasClass('bigText')) {
+      $('p').toggleClass().css('font-size', '28px');
+    } else {
+      $('p').toggleClass().css('font-size', '16px');
+    }
+  });
+};
 
-$('#largeText').click(() => {
-  if (h5.hasClass('bigText')) {
-    $('h5').toggleClass().css('font-size', '32px');
-  } else {
-    $('h5').toggleClass().css('font-size', '16px');
-  }
-});
+const nameFlipper = () => {
+  $('#largeText').click(() => {
+    if (h5.hasClass('bigText')) {
+      $('h5').toggleClass().css('font-size', '28px');
+    } else {
+      $('h5').toggleClass().css('font-size', '16px');
+    }
+  });
+};
+
+export default { fontFlipper, nameFlipper };
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -2,18 +2,19 @@ import messages from './components/displayMessage/messages';
 import themeChange from './components/themeChanger';
 import addMessage from './components/addMessage';
 import clear from './helpers/clear';
+import bigFont from './components/displayMessage/textChanger';
 import 'bootstrap';
 import '../styles/main.scss';
 import '../styles/_footer.scss';
-import text from './components/displayMessage/textChanger';
 
 const init = () => {
   messages.getMessages();
   themeChange.DarkOrLightModeEventListener();
   addMessage.addEventHandler();
   clear.clearMessages();
-  text.fontToggleEvent();
   messages.addDeleteBtnEventListener();
+  bigFont.fontFlipper();
+  bigFont.nameFlipper();
 };
 
 init();
