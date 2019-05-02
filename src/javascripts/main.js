6e1da26024486a419bf975a96df0f5c161fe49c6
Dev Davis <sean.dev.davis@gmail.com>
2019-05-01 19:08:09 -0500

    got the large text working

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -5,12 +5,14 @@ import clear from './helpers/clear';
 import 'bootstrap';
 import '../styles/main.scss';
 import '../styles/_footer.scss';
+import text from './components/displayMessage/textChanger';
 
 const init = () => {
   messages.getMessages();
   themeChange.DarkOrLightModeEventListener();
   addMessage.addEventHandler();
   clear.clearMessages();
+  text.fontToggleEvent();
 };
 
 init();
