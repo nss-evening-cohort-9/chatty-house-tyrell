2c230399767c98e2c6a0e99fb7bc0f81e89cea21
Dev Davis <sean.dev.davis@gmail.com>
2019-05-06 16:05:15 -0500

    wip

--- src/javascripts/components/displayMessage/textChanger.js
+++ src/javascripts/components/displayMessage/textChanger.js
@@ -3,9 +3,8 @@ import $ from 'jquery';
 const fontSize = [22, 16];
 
 const fontFlipper = () => {
-  $('input').click(() => {
+  $('clear-button').click(() => {
     $('p').css('font-size', fontSize[0]);
-    $('h5').css('font-size', fontSize[0]);
     fontSize.reverse();
   });
 };
