e297f1601b862d70aba4864a1721ed6b883ef0e2
Greg Stephen <GregAStephen@gmail.com>
2019-05-13 06:37:06 -0500

    WIP

--- src/javascripts/components/themeChanger.js
+++ src/javascripts/components/themeChanger.js
@@ -2,6 +2,8 @@ import $ from 'jquery';
 
 const body = $('body');
 const messages = $('#displayMessage');
+const modalContent = $('.modal-content');
+const modalCard = $('.theme-card');
 
 const DarkOrLightModeEventListener = () => {
   $('#darkOrLightMode').click(() => {
@@ -10,11 +12,19 @@ const DarkOrLightModeEventListener = () => {
       body.addClass('darkMode');
       messages.removeClass('lightModeMessages');
       messages.addClass('darkModeMessages');
+      modalContent.removeClass('lightMode');
+      modalContent.addClass('darkMode');
+      modalCard.removeClass('lightModeMessages');
+      modalCard.addClass('darkModeMessages');
     } else if (body.hasClass('darkMode')) {
       body.removeClass('darkMode');
       body.addClass('lightMode');
       messages.removeClass('darkModeMessages');
       messages.addClass('lightModeMessages');
+      modalContent.removeClass('darkMode');
+      modalContent.addClass('lightMode');
+      modalCard.removeClass('darkModeMessages');
+      modalCard.addClass('lightModeMessages');
     }
   });
 };
